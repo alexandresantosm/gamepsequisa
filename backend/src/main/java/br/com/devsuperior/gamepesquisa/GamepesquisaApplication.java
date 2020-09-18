eb0cb427a45ad7fa6daf5596c0b99397dfa83f50
alexandre <amenezes031@gmail.com>
2020-09-18 13:59:14 -0300

    Project created

---
+++ backend/src/main/java/br/com/devsuperior/gamepesquisa/GamepesquisaApplication.java
@@ -0,0 +1,13 @@
+package br.com.devsuperior.gamepesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamepesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamepesquisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/devsuperior/gamepesquisa/GamepesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devsuperior.gamepesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamepesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
