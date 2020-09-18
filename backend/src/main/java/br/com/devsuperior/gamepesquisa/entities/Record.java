06f02b0448f3d331636658a6d6de2556b8c10546
alexandresantosm <amenezes031@gmail.com>
2020-09-18 19:32:14 -0300

    Implement mapping of entities

--- backend/src/main/java/br/com/devsuperior/gamepesquisa/entities/Record.java
+++ backend/src/main/java/br/com/devsuperior/gamepesquisa/entities/Record.java
@@ -3,14 +3,29 @@ package br.com.devsuperior.gamepesquisa.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_records")
 public class Record implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private int age;
 	private Instant moment;
+	
+	@ManyToOne
+	@JoinColumn(name = "game_id")
 	private Game game;
 	
 	public Record() {}
