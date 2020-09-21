71ddccee554ed39aa053b9946b2665245beded25
alexandresantosm <amenezes031@gmail.com>
2020-09-21 17:32:10 -0300

    Implement route Home

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Header from './components/Header';
+import Home from './pages/Home';
+
+const Routes = () => (
+  <BrowserRouter>
+    <Header />
+    <Switch>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { ReactComponent as Logo } from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
 const Header = () => (
   <header className="header-container">
     <Logo />
-    <div>
+    <Link to="/">
       <div className="logo-text">
         <span className="logo-text-first">Big Game</span>
         <span className="logo-text-last"> Survey</span>
       </div>
-    </div>
+    </Link>
   </header>
 );
 
