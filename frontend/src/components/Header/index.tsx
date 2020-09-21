d45ce27032c4033f6936d44d6d6308568f2b3e3e
alexandre <amenezes031@gmail.com>
2020-09-21 13:00:08 -0300

    Implement component Header

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+
+import './styles.css';
+
+const Header = () => (
+  <header className="container">
+    <Logo />
+    <div>
+      <div className="logo-text">
+        <span className="logo-text-first">Big Game</span>
+        <span className="logo-text-last"> Survey</span>
+      </div>
+    </div>
+  </header>
+);
+
+export default Header;
